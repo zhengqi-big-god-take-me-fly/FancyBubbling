4a2868364e9ffdcfe583fe02615b19a6d9959cd8
perqin <perqinxie@gmail.com>
2016-06-26 02:23:04 +0800

    Move file and finish home logic

--- Classes/HomeScene/HomeScene.cpp
+++ Classes/HomeScene/HomeScene.cpp
@@ -26,12 +26,18 @@ bool HomeScene::init() {
 
 	// MenuLayer
 	auto startGameButton = MenuItemImage::create("start-game-normal.png", "start-game-pressed.png", CC_CALLBACK_1(HomeScene::onStartGameClick , this));
-    startGameButton->setPosition(vo.x, vo.y);
+    startGameButton->setAnchorPoint(Vec2(0, 0.5));
+    startGameButton->setPosition(vo.x + 64, vo.y + 240);
+    auto helpGameButton = MenuItemImage::create("help-game-normal.png", "help-game-pressed.png", CC_CALLBACK_1(HomeScene::onHelpGameClick, this));
+    helpGameButton->setAnchorPoint(Vec2(0, 0.5));
+    helpGameButton->setPosition(vo.x + 64, vo.y + 160);
 	auto aboutGameButton = MenuItemImage::create("about-game-normal.png", "about-game-pressed.png", CC_CALLBACK_1(HomeScene::onAboutGameClick, this));
-    aboutGameButton->setPosition(vo.x + vs.width / 2, vo.y + vs.height / 2);
+    aboutGameButton->setAnchorPoint(Vec2(1, 0.5));
+    aboutGameButton->setPosition(vo.x + vs.width - 64, vo.y + 240);
 	auto exitGameButton = MenuItemImage::create("exit-game-normal.png", "exit-game-pressed.png", CC_CALLBACK_1(HomeScene::onExitGameClick, this));
-    exitGameButton->setPosition(vo.x + vs.width, vo.y);
-	auto menuLayer = Menu::create(startGameButton, aboutGameButton, exitGameButton, nullptr);
+    exitGameButton->setAnchorPoint(Vec2(1, 0.5));
+    exitGameButton->setPosition(vo.x + vs.width - 64, vo.y + 160);
+	auto menuLayer = Menu::create(startGameButton, helpGameButton, aboutGameButton, exitGameButton, nullptr);
     menuLayer->setPosition(vo.x, vo.y);
 	addChild(menuLayer, 1);
 
@@ -43,7 +49,27 @@ void HomeScene::onStartGameClick(Ref * sender) {
     Director::getInstance()->pushScene(scene);
 }
 
-void HomeScene::onAboutGameClick(Ref * sender) {}
+void HomeScene::onHelpGameClick(Ref * sender) {
+    popupScreen = Sprite::create("game-help.png");
+    popupScreen->setPosition(vo.x + vs.width / 2, vo.y + vs.height / 2);
+    popupScreen->runAction(FadeIn::create(0.4f));
+    addChild(popupScreen, 5);
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->onTouchBegan = CC_CALLBACK_2(HomeScene::onPopupScreenTouchBegan, this);
+    touchListener->setSwallowTouches(true);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, popupScreen);
+}
+
+void HomeScene::onAboutGameClick(Ref * sender) {
+    popupScreen = Sprite::create("game-about.png");
+    popupScreen->setPosition(vo.x + vs.width / 2, vo.y + vs.height / 2);
+    popupScreen->runAction(FadeIn::create(0.4f));
+    addChild(popupScreen, 5);
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->onTouchBegan = CC_CALLBACK_2(HomeScene::onPopupScreenTouchBegan, this);
+    touchListener->setSwallowTouches(true);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, popupScreen);
+}
 
 void HomeScene::onExitGameClick(Ref * sender) {
 	Director::getInstance()->end();
@@ -51,4 +77,15 @@ void HomeScene::onExitGameClick(Ref * sender) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
 	exit(0);
 #endif
+}
+
+bool HomeScene::onPopupScreenTouchBegan(Touch * touch, Event * e) {
+    if (popupScreen != nullptr && popupScreen->getOpacity() == 255) {
+        popupScreen->runAction(Sequence::createWithTwoActions(FadeOut::create(0.4f), CallFuncN::create([this](Node * node) {
+            node->removeFromParent();
+            this->popupScreen = nullptr;
+        })));
+        return true;
+    }
+    return false;
 }
--- Classes/HomeScene/HomeScene.h
+++ Classes/HomeScene/HomeScene.h
@@ -8,13 +8,18 @@ USING_NS_CC;
 class HomeScene : public Layer {
 public:
 	static Scene * createScene();
-	CREATE_FUNC(HomeScene);
+	CREATE_FUNC(HomeScene)
 	virtual bool init();
 
 private:
+    // Callbacks
 	void onStartGameClick(Ref * sender);
+    void onHelpGameClick(Ref * sender);
 	void onAboutGameClick(Ref * sender);
 	void onExitGameClick(Ref * sender);
+    bool onPopupScreenTouchBegan(Touch * touch, Event * e);
+
+    Sprite * popupScreen;
 
 	Size vs;
 	Vec2 vo;
